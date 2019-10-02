c39a322389c508ee8edda485786373070a32d77b
Gayan <Gayan.mnc@gmail.com>
2019-10-02 23:39:37 +0530

    added singleton

--- ReadMe.txt
+++
@@ -1,4 +0,0 @@
-Basically, There are three types of design patterns.
-  1. Creational
-  2. Stuctural
-  3. Behavioral
---
+++ src/creational/singleton/eager/singleton_eager.java
@@ -0,0 +1,13 @@
+
+package creational.singleton.eager;
+
+public class singleton_eager {
+    
+    private static final singleton_eager instance = new singleton_eager();
+    
+    private singleton_eager(){}
+
+    public static singleton_eager getInstance(){
+        return instance;
+    }
+}
